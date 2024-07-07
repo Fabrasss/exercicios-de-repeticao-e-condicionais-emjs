44e478dbe6ce39491de13f3c8bff815d2c95ba52
Fabricio Almeida <fabas595653@gmail.com>
2024-07-07 20:56:55 -0300

    commitando

---
+++ exercicio-de-condicionais.js
@@ -0,0 +1,84 @@
+// Escreva um algoritmo em PORTUGOL que leia um número e informe se ele é divisível por
+// 10, por 5 ou por 2 ou se não é divisível por nenhum deles.
+// let numero = parseInt(prompt("Defina um número: "))
+
+// if (numero % 10 == 0) {
+//     console.log(numero + "é divisível por 10");
+// }else if (numero % 5 == 0) {
+//     console.log(numero + " é divisível por 5");
+// } else if (numero % 2 == 0) {
+//     console.log(numero + " é divisível por 2");    
+// } else {
+//     console.log(numero + " não é divisível por 10, 5 ou 2");
+// }
+
+
+// Construa um algoritmo de PORTUGOL para determinar se o indivíduo esta com um peso
+// favorável. Essa situação é determinada através do IMC (Índice de Massa Corpórea), que é
+// definida como sendo a relação entre o peso (PESO) e o quadrado da Altura (ALTURA) do
+// indivíduo. Ou seja,
+// //#####################################################################################################
+// let peso = parseFloat(prompt("Escreva seu peso "));
+// let altura = parseFloat(prompt("Escreva a sua altura: "))
+// let imc = peso / (altura * altura);
+//  if (imc < 20) {
+//     console.log("Situção: Abaixo do peso");
+//  } else if (imc > 20 || imc >= 25) {
+//    console.log("Situação: peso normal");   //questão dois
+//  } else if (imc > 25 || imc >= 30) {
+//    console.log("Situação: Sobre peso");
+// }  else if (imc > 30 || imc >= 40) {
+//    console.log("Situação: Obeso");  
+// }
+// console.log(imc);
+//###############################################################################################################
+
+// let saldoMedio = parseFloat(prompt("Digite o saldo médio do cliente:"));
+
+// let valorCredito = 0;
+
+
+// if (saldoMedio >= 0 && saldoMedio <= 500) {
+//     valorCredito = 0;
+// } else if (saldoMedio >= 501 && saldoMedio <= 1000) {
+//     valorCredito = 0.3;
+// } else if (saldoMedio >= 1001 && saldoMedio <= 3000) {
+//     valorCredito = 0.4 ;
+// } else if (saldoMedio >= 601 && saldoMedio <= 800) {
+//     valorCredito = 0.5;
+// }
+
+// console.log("Saldo Médio: R$ " + saldoMedio.toFixed(2));
+// console.log("Valor do Crédito Especial: R$ " + valorCredito.toFixed(2));
+// console.log(saldoMedio * valorCredito.toFixed(2));
+
+// ######################################################################
+
+
+// let idade = parseInt(prompt("Digite a idade da pessoa:"));
+
+
+// if (idade < 16) {
+//     console.log("Não eleitor.");
+// } else if ((idade >= 16 && idade <= 17) || idade > 65) { // <------ o uso de parentêse !!!!!!!
+//     console.log("Eleitor facultativo.");
+// } else if (idade >= 18 && idade <= 70) {
+//     console.log("Eleitor obrigatório.");
+// } else { // idade > 70
+//     console.log("Eleitor facultativo.");
+// }
+
+
+//########################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ exercicio-de-repeticao.js
@@ -0,0 +1,53 @@
+// for (let estudar = 0; estudar <= 20; estudar++) {
+//     console.log(estudar);
+    
+    
+// } primeira questão
+
+// let soma = 0;
+
+// for (let i = 1; i <= 15; i++) {
+//     soma += i;
+// }
+
+// console.log("A soma dos números de 1 a 15 é:", soma); //segunda questão
+
+
+// let nome = prompt("Digite seu nome: ");
+
+
+// for (let i = 0; i < 15; i++) {
+//     console.log(nome);
+// } //terceira questão
+
+// let nome = "fabricio"
+// let numeroDoUsuario = 1
+// let numero = 20
+
+// for (let i = 0; i <= 20;  i++) {
+//     console.log(nome, numeroDoUsuario, numero)
+    
+// }  //Questão 4
+
+
+// let soma = 0;
+
+
+// for (let cont = 1; cont <= 10; cont++) {
+//     let numero = parseFloat(prompt("Digite um número:"));
+//     soma += numero; 
+// }
+
+// console.log("A soma dos números é:", soma); // questão 5
+
+// soma = 0
+
+// for (let cont = 0; cont <= 20 ; cont++) {
+//     let numero = parseFloat(prompt("Digite a sua idade"))
+//     soma += numero
+    
+// }
+
+// console.log("A soma das idades é", soma) // questão 6
+
+
